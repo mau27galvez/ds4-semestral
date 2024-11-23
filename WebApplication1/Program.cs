b963a090b5b2e6f99fbf03d21084f944fca39388
Mauricio Galvez <mau27galvez@gmail.com>
2024-11-22 22:58:04 -0500

    configure cors

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -10,6 +10,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", b =>
+    {
+        b.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
